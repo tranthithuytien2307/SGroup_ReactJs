5b07fee76870c825ad947b494b89a98494d808eb
ThuyTien2307 <tranthithuytien2307@gmail.com>
2025-09-24 20:31:14 +0700

    feat: login-page and component/ui

---
+++ src/modules/LoginPage.tsx
@@ -0,0 +1,9 @@
+import LoginForm from "@/components/login/LoginForm";
+
+export default function LoginPage() {
+  return (
+    <div className="flex items-center justify-center h-screen ">
+      <LoginForm></LoginForm>
+    </div>
+  );
+}
